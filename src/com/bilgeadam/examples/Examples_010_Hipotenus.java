e53d9d39d0e8bb5fe14ddb0591c9a9ff6fbc094f
hamitmizrak <hamitmizrak@gmail.com>
2021-09-17 14:29:09 +0300

    Örneği: Hipotenüs

--- src/com/bilgeadam/examples/Examples_010_Hipotenus.java
+++ src/com/bilgeadam/examples/Examples_010_Hipotenus.java
@@ -3,23 +3,19 @@ package com.bilgeadam.examples;
 
 import java.util.Scanner;
 
+// Hipotenüs: a^2+b^2=c^2
+// 3*3+4*4=5*5
 public class Examples_010_Hipotenus {
 	
 	public static void main(String[] args) {
 		Scanner klavye = new Scanner(System.in);
-		System.out.println("Lütfen haftanın gününü giriniz");
-		int gun = klavye.nextInt();
-		/*
-		 * 1=pazartesi
-		 * 2=
-		 * 3=
-		 * 4=
-		 * 5=
-		 * 6=
-		 * 7=pazar
-		 */
-		
-		System.out.println("girdiğiniz gün:" + gun);
+		int sayi1, sayi2, sonuc;
+		System.out.println("Lütfen 1.sayıyı giriniz");
+		sayi1 = Math.abs(klavye.nextInt());
+		System.out.println("Lütfen 2.sayıyı giriniz");
+		sayi2 = Math.abs(klavye.nextInt());
+		sonuc = (int) Math.sqrt(((int) Math.pow(sayi1, 2)) + ((int) Math.pow(sayi2, 2)));
+		System.out.println(sayi1 + " " + sayi2 + " hipotenüs: " + sonuc);
 		
 	}
 	
